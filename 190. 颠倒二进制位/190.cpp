40cf3c0788b4a1362dc9d52e9bf806b84966525c
zhangpinge <50477615+warku123@users.noreply.github.com>
2021-03-29 11:31:04 +0800

    190

---
+++ 190. 颠倒二进制位/190.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t res=0;
+        bool tmp = 0;
+        for(unsigned i=0;i<32;i++){
+            res<<=1;
+            tmp = n&1;
+            n>>=1;
+            // cout<<tmp;
+            res = res+tmp;
+        }
+        return res;
+    }
+};
---
+++ 190. 颠倒二进制位/readme.md
@@ -0,0 +1,5 @@
+# 190. 颠倒二进制位
+
+https://leetcode-cn.com/problems/reverse-bits/
+
+类似队列，从末尾出，然后从头进，搞到二进制的位运算里面就是右移再左移。
